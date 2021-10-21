bcb00f0d67df5425b9572b8ed0bb84e5494c6d9e
alphatangojuliett <39340694+alphatangojuliett@users.noreply.github.com>
2021-10-20 18:15:37 -0600

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,16 @@
+===========
+Coupled Radio Interferometer
+===========
+
+For using the semi-analytic, first-order, antenna-antenna coupling formalism presented in [Array Element Coupling in Radio Interferometry I: A Semi-Analytic Approach](http://XXX) 
+
+## Installation
+
+Preferred method of installation for users is simply `pip install .`
+(or `pip install git+https://github.com/alphatangojuliett/CoupledRadioInterferometer`). This should install 
+required dependencies.
+
+## Tutorials
+
+We include two Jupyter notebooks: First and most importantly, [`tutorials/first_order_coupling_demo.ipynb`](tutorials/first_order_coupling_demo.ipynb), a demostration of the coupling formalism. Second, [`tutorials/airy_Efield_Beam_for_demo`](tutorials/airy_Efield_Beam_for_demo.ipynb), a brief 
+tutorial for how the beam object used in the demo was constructed. 
---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+from setuptools import setup, Extension, find_namespace_packages
+
+setup(
+    name='CoupledRadioInterferometer',
+    version='0.1.0',
+    author='A. T. Josaitis',
+    author_email='atj28@cam.ac.uk',
+    packages=find_namespace_packages(),
+    url='http://pypi.python.org/pypi/CoupledRadioInterferometer/',
+    license='LICENSE.txt',
+    description='For calculating and analyzing array element coupling effects in radio interferometers',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        'numpy>=1.15, <1.21',
+        'matplotlib>=2.2'
+        'pyuvdata',
+        'astropy>=2.0',
+        'healpy',
+        'ruamel.yaml',
+        'healvis @ git+git://github.com/rasg-affiliates/healvis@setup_all_bls_and_peak_norm_beam#egg=healvis',
+        'uvtools @ git+git://github.com/HERA-Team/uvtools@plot_range_control#egg=uvtools',
+        'hera_cal @ git+git://github.com/HERA-Team/hera_cal',
+        'hera_pspec @ git+git://github.com/alphatangojuliett/hera_pspec@atj-patch-1_uvtools_branch#egg=hera_pspec',
+    ],
+    include_package_data = True,
+)
